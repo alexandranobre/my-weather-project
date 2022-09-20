8fc416903c3c6ec75efd1213e0aaae84e4867bd0
alexandranobre <alexandra.nobre1992@gmail.com>
2022-09-20 16:57:04 +0100

    corrected the icon code

--- src/data.js
+++ src/data.js
@@ -30,9 +30,12 @@ function displayWeatherCondition(response) {
   document.querySelector("#wind").innerHTML = response.data.wind.speed;
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
-  document.querySelector(
-    "#icon"
-  ).innerHTML = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
 }
 
 function search(event) {
