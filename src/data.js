3773246b6106219eabe16b32fff2b066d43c39eb
alexandranobre <alexandra.nobre1992@gmail.com>
2022-09-30 12:34:42 +0100

    change the apiURL

--- src/data.js
+++ src/data.js
@@ -64,7 +64,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "33bf045b116ab19ff62ee0c20a979fdb";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
